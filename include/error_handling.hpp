5445c7b3ac98099c772f8fb0afc2929b1650f1bc
vhajsman <1816.cookie1816@gmail.com>
2026-07-08 12:23:53 +0200

    error handler

---
+++ include/error_handling.hpp
@@ -0,0 +1,50 @@
+#ifndef __ERROR_HANDLING_H
+#define __ERROR_HANDLING_H
+
+#include <exception>
+#include <string>
+
+namespace uil {
+    struct source_location {
+        std::string file_name;
+
+        unsigned int line;
+        unsigned int col;
+        unsigned int length;
+    };
+
+    class SyntaxEvent: public std::exception {
+        public:
+        enum class EventType {
+            ERROR,
+            WARNING,
+            NOTE
+        };
+
+        private:
+        EventType event_type;
+        source_location location;
+        std::string message;
+
+        unsigned int err_no;
+
+        public:
+        SyntaxEvent(
+            EventType event_type, 
+            const std::string& message, 
+            unsigned int err_no = 0, 
+            source_location location = {"", 0, 0, 0}
+        );
+
+        const char* what() const noexcept override {
+            return this->message.c_str();
+        }
+
+        const source_location& where() const;
+        const EventType& get_event_type() const;
+
+        std::string fmt(bool en_color = true, const std::string& line = "") const;
+    };
+};
+
+#endif
---
+++ src/error_handling.cpp
@@ -0,0 +1,74 @@
+#include "error_handling.hpp"
+#include "print_formatter.hpp"
+#include <iomanip>
+#include <sstream>
+
+namespace uil {
+    const source_location& SyntaxEvent::where() const {
+        return this->location;
+    }
+
+    const SyntaxEvent::EventType& SyntaxEvent::get_event_type() const {
+        return this->event_type;
+    }
+
+    std::string SyntaxEvent::fmt(bool en_color, const std::string& line) const {
+        //          | Error: test.txt
+        //      123 | int32 a = undefined_var + 14;
+        //          |           ^^^^^^^^^^^^^
+        //          | 'undefined_var' is not defined in this scope [E12]
+        //          |
+
+        std::stringstream out;
+        const char* type;
+        uint8_t color_idx;
+
+        switch(this->event_type) {
+            case EventType::ERROR:      type = "Error";   color_idx = COLOR_IDX_RED;    break;
+            case EventType::WARNING:    type = "Warning"; color_idx = COLOR_IDX_PURPLE; break;
+            case EventType::NOTE:       type = "Note";    color_idx = COLOR_IDX_CYAN;   break;
+        }
+
+        out << "           | ";
+
+        if(en_color) out << make_ansi_code(color_idx, true);
+        out << type;
+        if(en_color) out << ansi_reset();
+        out << " " << location.file_name << std::endl;
+
+        std::string line_number = std::to_string(this->location.line);
+
+        out << std::setw(10) << this->location.line << " | ";
+        if(!line.empty()) {
+            out << line << std::endl;
+            out << std::string(10, ' ') << " | ";
+    
+            for(unsigned int i = 1; i < this->location.col; i++)
+                out << " ";
+    
+            if(en_color) out << make_ansi_code(COLOR_IDX_RED, true);
+    
+            for(unsigned int i = 0; i < this->location.length; i++)
+                out << "^";
+        }
+
+        if(en_color) out << make_ansi_code();
+        out << "\n";
+        out << "           | ";
+        out << this->message;
+
+        if(this->err_no != 0)
+            out << " [E" << this->err_no << "]";
+
+        if(en_color) out << make_ansi_code();
+        out << "\n";
+        out << "           | " << std::endl;
+
+        return out.str();
+    }
+
+    SyntaxEvent::SyntaxEvent(SyntaxEvent::EventType event_type, const std::string& message, unsigned int err_no, source_location location)
+    : event_type(event_type), message(message), err_no(err_no), location(location) {
+
+    }
+};
