89f5d49424c4ca200501d34b1fce762634e4e915
vhajsman <1816.cookie1816@gmail.com>
2026-07-14 23:29:18 +0200

    stack -> vm stack

--- src/isa/push_pop.cpp
+++ src/isa/push_pop.cpp
@@ -3,7 +3,7 @@
 namespace uil {
     void VirtualMachine::push(uint64_t val) {
         if(regs.sp.value < stack_start + sizeof(uint64_t))
-            throw std::runtime_error("Stack overflow");
+            throw std::runtime_error("VM Stack overflow");
 
         regs.sp.value -= sizeof(uint64_t);
         write_memory(regs.sp.value, val);
@@ -11,7 +11,7 @@ namespace uil {
 
     uint64_t VirtualMachine::pop() {
         if(this->regs.sp.value + sizeof(uint64_t) > this->STACK_TOP)
-            throw std::runtime_error("Stack underflow");
+            throw std::runtime_error("VM Stack underflow");
 
         uint64_t val = this->read_memory(this->regs.sp.value);
         this->regs.sp.value += sizeof(uint64_t);
